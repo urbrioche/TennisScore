57086f49434e3a032a43142761438e0f4adaeacd
urbrioche <aafulin@gmail.com>
2018-03-05 22:19:11 +0800

    test case deuce

--- TennisScore/TennisGame.cs
+++ TennisScore/TennisGame.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TennisScore
 {
     public class TennisGame
     {
         private readonly IRepository<Game> _repo;
+        private static Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"}
+        };
 
         public TennisGame(IRepository<Game> repo)
         {
@@ -15,7 +23,19 @@ namespace TennisScore
         {
             var game = this._repo.GetGame(gameId);
 
+            if (game.FirstPlayerScore == game.SecondPlayerScore && game.FirstPlayerScore == 3)
+            {
+                return "Deuce";
+            }
+
+            if (game.FirstPlayerScore == game.SecondPlayerScore)
+            {
+                return  _scoreLookup[game.FirstPlayerScore] + " All";
+            }
+            
+
             throw new NotImplementedException();
+
         }
     }
 }
